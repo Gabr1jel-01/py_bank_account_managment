2ff5e0cf1104167364d4f72d500954645945428d
Gabrijel <g.gordic@outlook.com>
2023-03-20 21:11:45 +0100

    Update py_pwd_generator.py

---
+++ py_pwd_generator.py
@@ -0,0 +1,34 @@
+password_lenght = 25
+
+
+# deklaracije
+def generate_password():
+    password_lenght = 50
+    print(f'password_lenght u funkciji: {password_lenght}') #50
+ 
+
+
+
+# main 
+
+print(f'Prije poziva funkcije: {password_lenght}') #25
+generate_password()
+print(f'Poslije poziva funkcije: {password_lenght}') # 25
+
+
+# od 33 do 126 i 126 u ascii table
+# od 33 do 47 znakovi
+# od 48 do 57 brojevi
+# od 58 do 64 znakovi 
+# od 65 do 90 velika slova
+# od 91 do 96 znakovi
+# od 97 do 122 mala slova
+# od 123 do 126 znakovi
+
+
+
+
+
+# end
+#print(password)
+
