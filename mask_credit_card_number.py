525e5b707ea5d6d9210c431e758b7072295ccb42
Gabrijel <g.gordic@outlook.com>
2023-03-20 20:22:17 +0100

    Update mask_credit_card_number.py

--- mask_credit_card_number.py
+++ mask_credit_card_number.py
@@ -3,17 +3,23 @@
 
 
 #deklaracija
-def mask_ccn(credit_card_number: str, masking_symbol: str) -> str:
+def mask_ccn(ccn: str = '', 
+             masking_symbol: str = '#',
+             masking_limit_numbers: int = 4) -> str:
     masked_ccn = ''
     
-    lenght_credit_card_number = credit_card_number[ : -4]                
+    if ccn == '':
+        print('Broj kreditne kartice ne moze biti prazan!')
+        return # kao break u petlji prekida izvrsavanje funkcije
+    
+    lenght_credit_card_number = ccn[ : masking_limit_numbers]                
     for masked_number in lenght_credit_card_number:
         if masked_number == '-':
             masked_ccn += masked_number
         else:
             masked_ccn += masking_symbol
 
-    masked_ccn += credit_card_number[-4 : ]
+    masked_ccn += ccn[masking_limit_numbers : ]
     
     return masked_ccn
     
@@ -21,9 +27,12 @@ def mask_ccn(credit_card_number: str, masking_symbol: str) -> str:
 #main
 credit_card_number = input('Upisite broj kreditne kartice: ')
 masking_symbol = input('Upisite simbol sa kojim zelite zamaskirati brojeve: ')
-masked_credit_card_number = mask_ccn(credit_card_number, masking_symbol)
+#masking_symbol = '_'
+masking_limit_numbers = int(input('Upisite limit maskiranja broja kartice: '))
+masked_credit_card_number = mask_ccn(ccn=credit_card_number, masking_limit_numbers=masking_limit_numbers)
 
 #end
 print(f'Broj kreditne kartice {credit_card_number}')
-print(f'Maskirani broj kreditne kartice: {masked_credit_card_number}')
-
+if masked_credit_card_number != '': 
+    print(f'Maskirani broj kreditne kartice: {masked_credit_card_number}')
+print()
